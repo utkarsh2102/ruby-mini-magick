d7e1bb8135e8b4327357c229203e9bcdb981ae7c
markus tornow <tornow@riseup.net>
2013-05-24 13:11:23 +0200

    Imported Upstream version 3.6.0

--- test/command_builder_test.rb
+++ test/command_builder_test.rb
@@ -3,6 +3,16 @@ require 'test_helper'
 class CommandBuilderTest < Test::Unit::TestCase
   include MiniMagick
 
+  def setup
+    @processor_path = MiniMagick.processor_path
+    @processor = MiniMagick.processor
+  end
+
+  def teardown
+    MiniMagick.processor_path = @processor_path
+    MiniMagick.processor = @processor
+  end
+
   def test_basic
     c = CommandBuilder.new("test")
     c.resize "30x40"
@@ -26,7 +36,7 @@ class CommandBuilderTest < Test::Unit::TestCase
   def test_plus_modifier_and_multiple_options
     c = CommandBuilder.new("test")
     c.distort.+ 'srt', '0.6 20'
-    assert_equal '+distort srt 0.6\ 20', c.args.join(" ")
+    assert_equal '\+distort srt 0.6\ 20', c.args.join(" ")
   end
 
   def test_valid_command
@@ -63,4 +73,18 @@ class CommandBuilderTest < Test::Unit::TestCase
     assert_equal 'test -set colorspace\ RGB', c.command
   end
 
+  def test_processor_path
+    MiniMagick.processor_path = "/a/strange/path"
+    c = CommandBuilder.new('test')
+    c.auto_orient
+    assert_equal c.command, "/a/strange/path/test -auto-orient"
+  end
+
+  def test_processor_path_with_processor
+    MiniMagick.processor_path = "/a/strange/path"
+    MiniMagick.processor = "processor"
+    c = CommandBuilder.new('test')
+    c.auto_orient
+    assert_equal c.command, "/a/strange/path/processor test -auto-orient"
+  end
 end
